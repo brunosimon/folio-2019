a5b913ebfe511fad487dbf07ae1a0decab72cdf7
brunosimon <simon.bruno.77@gmail.com>
2019-08-27 23:55:04 +0200

    :sparkles: Add scroll zoom

--- src/javascript/Camera.js
+++ src/javascript/Camera.js
@@ -12,10 +12,10 @@ export default class Camera
         this.debug = _options.debug
 
         // Set up
-        this.basePosition = new THREE.Vector3(8, - 8, 12)
-        this.position = new THREE.Vector3(0, 0, 0)
+        this.invertDirection = new THREE.Vector3(10.5, - 10.5, 15.5).normalize()
         this.target = new THREE.Vector3(0, 0, 0)
-        this.easing = 0.25
+        this.targetEased = new THREE.Vector3(0, 0, 0)
+        this.easing = 0.15
 
         // Debug
         if(this.debug)
@@ -25,6 +25,7 @@ export default class Camera
         }
 
         this.setInstance()
+        this.setZoom()
         this.setOrbitControls()
 
         // Debug
@@ -39,7 +40,7 @@ export default class Camera
         // Set up
         this.instance = new THREE.PerspectiveCamera(40, this.sizes.viewport.width / this.sizes.viewport.height, 2, 50)
         this.instance.up.set(0, 0, 1)
-        this.instance.position.copy(this.basePosition)
+        this.instance.position.copy(this.invertDirection)
         this.instance.lookAt(new THREE.Vector3())
 
         // Resize event
@@ -54,14 +55,40 @@ export default class Camera
         {
             if(!this.orbitControls.enabled)
             {
-                this.position.x += (this.target.x - this.position.x) * this.easing
-                this.position.y += (this.target.y - this.position.y) * this.easing
-                this.position.z += (this.target.z - this.position.z) * this.easing
+                this.targetEased.x += (this.target.x - this.targetEased.x) * this.easing
+                this.targetEased.y += (this.target.y - this.targetEased.y) * this.easing
+                this.targetEased.z += (this.target.z - this.targetEased.z) * this.easing
 
-                this.instance.position.copy(this.position).add(this.basePosition)
+                this.instance.position.copy(this.targetEased).add(this.invertDirection.clone().multiplyScalar(this.zoom.distance))
 
-                this.instance.lookAt(this.position)
+                this.instance.lookAt(this.targetEased)
             }
+        })
+    }
+
+    setZoom()
+    {
+        // Set up
+        this.zoom = {}
+        this.zoom.easing = 0.1
+        this.zoom.baseDistance = 20
+        this.zoom.distance = this.zoom.baseDistance
+        this.zoom.amplitude = 10
+        this.zoom.value = 0
+        this.zoom.targetValue = 0
+
+        // Listen to mousewheel event
+        document.addEventListener('mousewheel', (_event) =>
+        {
+            this.zoom.targetValue += _event.deltaY * 0.001
+            this.zoom.targetValue = Math.min(Math.max(this.zoom.targetValue, - 1), 1)
+        }, { passive: true })
+
+        // Time tick event
+        this.time.on('tick', () =>
+        {
+            this.zoom.value += (this.zoom.targetValue - this.zoom.value) * this.zoom.easing
+            this.zoom.distance = this.zoom.baseDistance + this.zoom.value * this.zoom.amplitude
         })
     }
 
