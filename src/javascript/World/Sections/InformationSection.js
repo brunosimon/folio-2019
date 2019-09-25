69f07260993b7a75de7453d04e023e565d552daa
brunosimon <simon.bruno.77@gmail.com>
2019-09-25 14:10:16 +0200

    :sparkles: Information > Add areas

--- src/javascript/World/Sections/InformationSection.js
+++ src/javascript/World/Sections/InformationSection.js
@@ -18,11 +18,12 @@ export default class InformationSection
         this.container.updateMatrix()
 
         this.x = 0
-        this.y = - 55
-        // this.y = - 10
+        // this.y = - 55
+        this.y = - 10
 
         this.setStatic()
         this.setBaguettes()
+        this.setLinks()
         this.setTiles()
     }
 
@@ -63,6 +64,78 @@ export default class InformationSection
             shadow: { sizeX: 0.6, sizeY: 3.5, offsetZ: - 0.15, alpha: 0.35 },
             mass: 1.5,
             sleep: false
+        })
+    }
+
+    setLinks()
+    {
+        // Set up
+        this.links = {}
+        this.links.x = 1.45
+        this.links.y = - 1.5
+        this.links.halfExtents = {}
+        this.links.halfExtents.x = 1
+        this.links.halfExtents.y = 1
+        this.links.distanceBetween = 2.4
+
+        // Twitter
+        this.links.twitter = {}
+        this.links.twitter.x = this.x + this.links.x + this.links.distanceBetween * 0
+        this.links.twitter.y = this.y + this.links.y
+        this.links.twitter.href = 'https://twitter.com/bruno_simon/'
+
+        this.links.twitter.area = this.areas.add({
+            position: new THREE.Vector2(this.links.twitter.x, this.links.twitter.y),
+            halfExtents: new THREE.Vector2(this.links.halfExtents.x, this.links.halfExtents.y)
+        })
+        this.links.twitter.area.on('interact', () =>
+        {
+            window.open(this.links.twitter.href, '_blank')
+        })
+
+        // Github
+        this.links.github = {}
+        this.links.github.x = this.x + this.links.x + this.links.distanceBetween * 1
+        this.links.github.y = this.y + this.links.y
+        this.links.github.href = 'https://github.com/brunosimon/'
+
+        this.links.github.area = this.areas.add({
+            position: new THREE.Vector2(this.links.github.x, this.links.github.y),
+            halfExtents: new THREE.Vector2(this.links.halfExtents.x, this.links.halfExtents.y)
+        })
+        this.links.github.area.on('interact', () =>
+        {
+            window.open(this.links.github.href, '_blank')
+        })
+
+        // Linkedin
+        this.links.linkedin = {}
+        this.links.linkedin.x = this.x + this.links.x + this.links.distanceBetween * 2
+        this.links.linkedin.y = this.y + this.links.y
+        this.links.linkedin.href = 'https://www.linkedin.com/in/simonbruno77/'
+
+        this.links.linkedin.area = this.areas.add({
+            position: new THREE.Vector2(this.links.linkedin.x, this.links.linkedin.y),
+            halfExtents: new THREE.Vector2(this.links.halfExtents.x, this.links.halfExtents.y)
+        })
+        this.links.linkedin.area.on('interact', () =>
+        {
+            window.open(this.links.linkedin.href, '_blank')
+        })
+
+        // Mail
+        this.links.mail = {}
+        this.links.mail.x = this.x + this.links.x + this.links.distanceBetween * 3
+        this.links.mail.y = this.y + this.links.y
+        this.links.mail.href = 'mailto:simon.bruno.77@gmail.com'
+
+        this.links.mail.area = this.areas.add({
+            position: new THREE.Vector2(this.links.mail.x, this.links.mail.y),
+            halfExtents: new THREE.Vector2(this.links.halfExtents.x, this.links.halfExtents.y)
+        })
+        this.links.mail.area.on('interact', () =>
+        {
+            window.open(this.links.mail.href, '_blank')
         })
     }
 
